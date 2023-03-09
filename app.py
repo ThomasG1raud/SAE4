c2396c3f844d3a4e7deeb2113680b446965b269c
tmoumen <taha.moumen@edu.univ-fcomte.fr>
2023-03-09 12:46:50 +0100

    supression de certaines colonnes

--- app.py
+++ app.py
@@ -1,48 +1,99 @@
-# importing flask
+# # importing flask
+# from flask import Flask, render_template
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import seaborn as sns
+# import pandas as pd
+#
+#
+# app = Flask(__name__)
+#
+# meteo = pd.read_csv('donneMeteo.csv',sep=';')
+# meteo1=meteo.head()
+# # print(meteo1.info())
+#
+# ########   analyse de la temperature par region #########
+# sns.relplot(x="region (name)", y="Température (°C)", data=meteo, kind="line")
+# plt.xticks(rotation=90)
+# plt.show()
+# ################################################################
+#
+# # route to html page - "table"
+# @app.route('/')
+# def page():
+#     return render_template('principalPage.html')
+#
+#
+#
+# @app.route('/tableMeteo')
+# def tableMeteo():
+#     # converting csv to html
+#     meteo = pd.read_csv('donneMeteo.csv',sep=';')
+#     meteo1=meteo.head()
+#     return render_template('tableMeteo.html', tables=[meteo1.to_html()], titles=[''])
+#
+#
+# @app.route('/tableAir')
+# def tableAir():
+#     # converting csv to html
+#     air = pd.read_csv('openaq.csv',sep=';')
+#     air1=air.head()
+#     return render_template('tableAir.html', tables=[air1.to_html()], titles=[''])
+
+
+
 from flask import Flask, render_template
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
-  
-  
+
+
 app = Flask(__name__)
-  
-meteo = pd.read_csv('donneMeteo.csv',sep=';', index_col='Date', parse_dates=True)
-air = pd.read_csv('openaq.csv', sep=';', index_col='Last Updated', parse_dates=True)
-meteoAir = pd.concat([meteo, air])
-#meteoAir.reset_index(inplace=True, drop=True)
+
+# Lire les fichiers CSV
+meteo = pd.read_csv('donneMeteo.csv',sep=',')
+#     meteo1=meteo.head()
+air = pd.read_csv('openaq.csv',sep=',')
+#     air1=air.head()
+
+# Nettoyer les données
+
+#     meteo.dropna(inplace=True)
+print(meteo.columns)
+# cols_to_drop_meteo = ['Pression station','Nébulosité couche nuageuse 2','Type de tendance barométrique.1']
+
+# meteo.drop(columns=cols_to_drop_meteo, inplace=True)
+# meteo.to_csv('donneMeteo.csv', index=False)
 
 
 
-# route to html page - "table"
+# #     air.dropna(inplace=True)
+print(air.columns)
+# cols_to_drop_air = ['Source Name', 'Country Code','Location']
+# air.drop(columns=cols_to_drop_air, inplace=True)
+# air.to_csv('openaq.csv', index=False)
+
+
+
+# Fusionner les cadres de données
+# merged_df = pd.merge(meteo, air, on='id')
+
 @app.route('/')
 def page():
     return render_template('principalPage.html')
-    
 
 
 @app.route('/tableMeteo')
 def tableMeteo():
-    # converting csv to html
-    return render_template('tableMeteo.html', tables=[meteo.head().to_html()], titles=[''])
+    meteo1=meteo.head()
+    return render_template('tableMeteo.html', tables=[meteo1.to_html()], titles=[''])
 
 
 @app.route('/tableAir')
 def tableAir():
-    # converting csv to html
-    return render_template('tableAir.html', tables=[air.head().to_html()], titles=[''])
-
-
-@app.route('/tableMeteoAir')
-def tableMeteoAir():
-    # converting csv to html
-    return render_template('tableMeteoAir.html', tables=[meteoAir.head().to_html()], titles=[''])
-   
-
-
-
+    air1=air.head()
+    return render_template('tableAir.html', tables=[air1.to_html()], titles=[''])
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
