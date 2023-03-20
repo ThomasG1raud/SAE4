f7a775b70a12858c94e0587c08840cf783e8cb6e
ThomasG1raud <91986615+ThomasG1raud@users.noreply.github.com>
2023-03-20 16:20:36 +0100

    merge

--- app.py
+++ app.py
@@ -8,24 +8,25 @@ import pandas as pd
 app = Flask(__name__)
 
 # Lire les fichiers CSV
-meteo = pd.read_csv('meteo.csv',sep=';')
+meteo = pd.read_csv('meteo.csv', sep=';', index_col="Date", parse_dates=True)
+meteo.index = pd.to_datetime(meteo.index, utc=True)
+air = pd.read_csv('air.csv', sep=',', index_col="Date", parse_dates=True)
 
-air = pd.read_csv('air.csv',sep=',')
-# print(air.columns)
+
 
 #########     choix colonnes meteo   ###########################
-meteo1 = meteo[["ID OMM station", "Date", "Humidité", "Nom", "Température (°C)", "Altitude", "communes (name)", "communes (code)", "EPCI (name)", "department (name)", "region (name)", "mois_de_l_annee"]]
-df = meteo1[meteo1["region (name)"] == "Centre-Val de Loire"]
-nb_lignes = df.shape[0]
-print("Nombre de lignes pour la région Centre-Val de Loire :",nb_lignes)
-# meteo1.to_csv('meteo1.csv', index=False)
+meteo = meteo[["ID OMM station", "Humidité", "Nom", "Température (°C)", "Altitude", "communes (name)", "communes (code)", "EPCI (name)", "department (name)", "region (name)", "mois_de_l_annee"]]
+meteo = meteo[meteo["region (name)"] == "Centre-Val de Loire"]
+nb_lignes = meteo.shape[0]
+print("Nombre de lignes pour la région Centre-Val de Loire :", nb_lignes)
 
 #########       choix colonnes air #################
-air1 = air[["date_ech","lib_qual", "lib_zone", "code_no2", "code_so2", "code_o3", "code_pm10", "code_pm25", "conc_no2", "conc_so2", "conc_o3", "conc_pm10", "conc_pm25"]]
-nb_lignes_air=air1.shape[0]
-print("Nombre de lignes pour la région Centre-Val de Loire :",nb_lignes_air)
-# air1.to_csv('air1.csv', index=False)
+air = air[["lib_qual", "lib_zone", "code_no2", "code_so2", "code_o3", "code_pm10", "code_pm25", "conc_no2", "conc_so2", "conc_o3", "conc_pm10", "conc_pm25"]]
+nb_lignes_air = air.shape[0]
+print("Nombre de lignes pour la région Centre-Val de Loire :", nb_lignes_air)
 
+
+meteo_air = pd.merge(meteo, air, on="Date")
 @app.route('/')
 def page():
     return render_template('principalPage.html')
@@ -33,16 +34,16 @@ def page():
 
 @app.route('/tableMeteo')
 def tableMeteo():
-    return render_template('tableMeteo.html', tables=[df.head().to_html()], titles=[''])
+    return render_template('tableMeteo.html', tables=[meteo.tail().to_html()], titles=[''])
 
 
 @app.route('/tableAir')
 def tableAir():
-    return render_template('tableAir.html', tables=[air1.tail().to_html()], titles=[''])
+    return render_template('tableAir.html', tables=[air.tail().to_html()], titles=[''])
 
-# @app.route('/tableMeteoAir')
-# def tableMeteoAir():
-#     return render_template('tableMeteo.html', tables=[meteo_air.to_html()], titles=[''])
+@app.route('/tableMeteoAir')
+def tableMeteoAir():
+    return render_template('tableMeteo.html', tables=[meteo_air.head().to_html()], titles=[''])
 
 if __name__ == '__main__':
     app.run(debug=True)
